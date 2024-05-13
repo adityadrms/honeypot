44fc4c33be60f499121ebb72625e05163a4ec38e
adityadrms <88520101+adityadrms@users.noreply.github.com>
2024-05-13 11:22:29 +0700

    Update paramiko.py

--- paramiko.py
+++ paramiko.py
@@ -1,37 +1,25 @@
 import paramiko
 
-def ssh_command(hostname, username, password, command):
-    # Inisialisasi klien SSH
-    ssh_client = paramiko.SSHClient()
-    # Setel kebijakan verifikasi host otomatis
-    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+# Buat objek SSHClient
+ssh = paramiko.SSHClient()
 
-    try:
-        # Koneksi ke host
-        ssh_client.connect(hostname, username=username, password=password)
+# Secara default, klien tidak akan terhubung ke host yang tidak dikenali
+# Tetapi Anda dapat mengatur opsi yang memungkinkan untuk otomatis menambahkan host ke known_hosts
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-        # Eksekusi perintah SSH
-        stdin, stdout, stderr = ssh_client.exec_command(command)
-
-        # Baca output
-        output = stdout.read().decode()
-
-        # Cetak output
-        print(output)
-
-    except paramiko.AuthenticationException:
-        print("Authentication failed, please verify your credentials")
-    except paramiko.SSHException as sshException:
-        print("Unable to establish SSH connection: %s" % sshException)
-    finally:
-        # Tutup koneksi
-        ssh_client.close()
-
-# Konfigurasi SSH
-hostname = '192.168.134.35'
+# Masukkan informasi koneksi SSH (ganti dengan info Raspberry Pi Anda)
+hostname = '192.168.40.35'
 username = 'raspberry'
 password = '123'
-command = 'ls -l'
 
-# Panggil fungsi untuk menjalankan perintah SSH
-ssh_command(hostname, username, password, command)
+# Terhubung ke host
+ssh.connect(hostname, username=username, password=password)
+
+# Lakukan sesuatu setelah terhubung, misalnya eksekusi perintah
+stdin, stdout, stderr = ssh.exec_command('ls -l')
+
+# Baca output dari perintah
+print(stdout.read().decode())
+
+# Tutup koneksi SSH
+ssh.close()
