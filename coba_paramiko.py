43e8d2a41e58e6db1e31dd0a02bbb67605c61678
adityadrms <88520101+adityadrms@users.noreply.github.com>
2024-05-13 11:46:33 +0700

    Update coba_paramiko.py

--- coba_paramiko.py
+++ coba_paramiko.py
@@ -1,26 +1,32 @@
 import paramiko
-import paramiko.util
+import sys
 
-# Aktifkan debug
+# Aktifkan mode debug dan atur tingkat log
 paramiko.util.log_to_file('paramiko.log')
-paramiko.util.log_level(paramiko.util.DEBUG)
+paramiko.common.logging.basicConfig(level=paramiko.common.DEBUG)
 
 # Buat objek SSHClient
 ssh = paramiko.SSHClient()
 
-# Secara default, klien tidak akan terhubung ke host yang tidak dikenali
-# Tetapi Anda dapat mengatur opsi yang memungkinkan untuk otomatis menambahkan host ke known_hosts
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
-# Masukkan informasi koneksi SSH (ganti dengan info Raspberry Pi Anda)
-hostname = '192.168.40.35'
-username = 'raspberry'
-password = '123'
-
 try:
-    # Coba melakukan koneksi
+    # Ganti dengan informasi koneksi Anda
+    hostname = '192.168.40.35'
+    username = 'raspberry'
+    password = '123'
+
+    # Secara default, klien tidak akan terhubung ke host yang tidak dikenali
+    # Tetapi Anda dapat mengatur opsi yang memungkinkan untuk otomatis menambahkan host ke known_hosts
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    # Terhubung ke host
     ssh.connect(hostname, username=username, password=password)
     print("Koneksi berhasil!")
+
+    # Lakukan sesuatu setelah terhubung, misalnya eksekusi perintah
+    stdin, stdout, stderr = ssh.exec_command('ls -l')
+
+    # Baca output dari perintah
+    print(stdout.read().decode())
 except Exception as e:
     print("Koneksi gagal:", str(e))
 finally:
