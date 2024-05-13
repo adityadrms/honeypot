4918c371f37994519086a770f932215f08ac75f3
adityadrms <88520101+adityadrms@users.noreply.github.com>
2024-05-13 11:39:49 +0700

    Update paramiko.py

--- paramiko.py
+++ paramiko.py
@@ -12,22 +12,27 @@ hostname = '192.168.40.35'
 username = 'raspberry'
 password = '123'
 
-# Terhubung ke host
-ssh.connect(hostname, username=username, password=password)
-
 try:
-    # Coba melakukan koneksi
+    # Ganti dengan informasi koneksi Anda
+    hostname = 'alamat_IP_Raspberry_Pi'
+    username = 'username_ssh'
+    password = 'password_ssh'
+
+    # Secara default, klien tidak akan terhubung ke host yang tidak dikenali
+    # Tetapi Anda dapat mengatur opsi yang memungkinkan untuk otomatis menambahkan host ke known_hosts
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    # Terhubung ke host
     ssh.connect(hostname, username=username, password=password)
     print("Koneksi berhasil!")
+
+    # Lakukan sesuatu setelah terhubung, misalnya eksekusi perintah
+    stdin, stdout, stderr = ssh.exec_command('ls -l')
+
+    # Baca output dari perintah
+    print(stdout.read().decode())
 except Exception as e:
     print("Koneksi gagal:", str(e))
 finally:
-
-# Lakukan sesuatu setelah terhubung, misalnya eksekusi perintah
-stdin, stdout, stderr = ssh.exec_command('ls -l')
-
-# Baca output dari perintah
-print(stdout.read().decode())
-
-# Tutup koneksi SSH
-ssh.close()
+    # Tutup koneksi SSH
+    ssh.close()
